e4279212a082f509188ad2d9724364080ca39f55
Karina Chan <karinachan902@gmail.com>
2017-11-17 23:11:49 -0800

    added more

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() + '/public'));
+//app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+
+
+app.use(methodOverride('_method'));
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({
+	defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+var routes = require('./controllers/burgers_controller.js');
+app.use('/', routes);
+
+
+app.set('port', (process.env.PORT || 3000));
+
+app.listen(app.get('port'), function () {
+	console.log('App listening on PORT ', app.get('port'));
+});
